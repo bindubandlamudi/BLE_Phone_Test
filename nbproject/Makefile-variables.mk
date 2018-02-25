d1c41738156155a7bd3037812a7c9b7269c60b91
Bindu Bandlamudi <bin0212@gmail.com>
2018-02-25 12:41:32 -0700

    MLDP Added + switch + 100ms delay - only this combi works

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=ble_test-master.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/ble_test-master.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=bletest-master.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/bletest-master.x.tar
